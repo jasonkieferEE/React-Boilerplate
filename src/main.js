3b3037cc7c8ab612361a1dc686086a3dff36b21d
jason kiefer <jason.kiefer@outlook.com>
2020-06-13 14:17:53 -0400

    Package cleanup

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+// SCSS Framework
+import './assets/scss/index.scss';
+
+// Bootstrap
+import 'bootstrap-css-only/css/bootstrap.css';
+
 if (module.hot) module.hot.accept();
 
 ReactDOM.render(
